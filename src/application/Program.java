2246ff40f9f6f5f0b1fd37bb628eecf7d0d2c77d
NatanRei <nreischmura@gmail.com>
2023-07-17 16:10:31 -0300

    feat: department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,9 +2,14 @@ package application;
 
 import java.sql.SQLException;
 
+import model.entities.Department;
+
 public class Program {
 
 	public static void main(String[] args) throws SQLException {
+		
+		Department obj = new Department(1, "Books");
+		System.out.println(obj);
 		
 	}
 
